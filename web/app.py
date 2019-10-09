fc1dda368874da809e5abf93eeea8a0b60888eb8
James Addison <55152140+jayaddison@users.noreply.github.com>
2019-10-09 11:01:18 -0700

    Add flake8

--- web/app.py
+++ web/app.py
@@ -1,9 +1,9 @@
 from os import path
 
 from flask import Flask, abort
-
 import requests
-from requests.exceptions import ConnectionError, ReadTimeout
+
+
 def request_patch(self, *args, **kwargs):
     kwargs['proxies'] = {
         'http': 'http://localhost:3128',
@@ -12,7 +12,12 @@ def request_patch(self, *args, **kwargs):
     kwargs['timeout'] = kwargs.pop('timeout', 5)
     kwargs['verify'] = '/etc/squid/certificates/ca.crt'
     return self.request_orig(*args, **kwargs)
-setattr(requests.sessions.Session, 'request_orig', requests.sessions.Session.request)
+
+
+setattr(
+    requests.sessions.Session, 'request_orig',
+    requests.sessions.Session.request
+)
 requests.sessions.Session.request = request_patch
 
 
@@ -22,7 +27,10 @@ app = Flask(__name__)
 @app.route('/<path:recipe_path>')
 def root(recipe_path):
     recipe_id = path.basename(recipe_path)
-    recipe = request.get('http://api-service', params={'recipe_id': recipe_id})
+    recipe = requests.get(
+        url='http://api-service',
+        params={'recipe_id': recipe_id}
+    )
 
     try:
         recipe.raise_for_status()
@@ -30,7 +38,7 @@ def root(recipe_path):
         return abort(404)
 
     image_src = recipe.json().get('image_src')
-    image = request.get(image_url)
+    image = requests.get(image_src)
 
     try:
         image.raise_for_status()
