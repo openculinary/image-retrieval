0a53703e01acdff7440c98dd2e686e78fd611a84
James Addison <james@reciperadar.com>
2020-05-27 18:03:20 +0100

    Revert "Remove request-patching magic"
    This reverts commit 6e1e781474f92ee3abb5c046b38973140e6e7675.

--- web/app.py
+++ web/app.py
@@ -4,6 +4,23 @@ from flask import Flask, abort
 import requests
 
 
+def request_patch(self, *args, **kwargs):
+    kwargs['proxies'] = kwargs.pop('proxies', {
+        'http': 'http://proxy:3128',
+        'https': 'http://proxy:3128',
+    })
+    kwargs['timeout'] = kwargs.pop('timeout', 5)
+    kwargs['verify'] = kwargs.pop('verify', '/etc/ssl/k8s/proxy-cert/ca.crt')
+    return self.request_orig(*args, **kwargs)
+
+
+setattr(
+    requests.sessions.Session, 'request_orig',
+    requests.sessions.Session.request
+)
+requests.sessions.Session.request = request_patch
+
+
 app = Flask(__name__)
 
 
@@ -11,14 +28,22 @@ app = Flask(__name__)
 def recipe(image_filename):
     recipe_id, extension = path.splitext(image_filename)
 
-    recipe = requests.get(f'http://api-service/api/recipes/{recipe_id}')
+    recipe = requests.get(
+        url=f'http://api-service/api/recipes/{recipe_id}',
+        proxies={}
+    )
+
     try:
         recipe.raise_for_status()
     except Exception:
         return abort(404)
 
     image_src = recipe.json().get('image_src')
-    image = requests.get(f'http://imageproxy/192,png/{image_src}')
+    image = requests.get(
+        url=f'http://imageproxy/192,png/{image_src}',
+        proxies={}
+    )
+
     try:
         image.raise_for_status()
     except Exception:
