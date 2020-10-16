02d2d455d9f86785eeff957e9485d9c6d3bf58da
James Addison <james@reciperadar.com>
2020-10-16 12:40:43 +0100

    Allow retrieval of per-domain icons from custom URLs (#6)

--- tests/test_root.py
+++ tests/test_root.py
@@ -8,7 +8,32 @@ IMAGEPROXY_URL = 'http://imageproxy'
 
 
 @pytest.fixture
-def imageproxy_response(image_path):
+def domain_responses(image_path):
+    image_filename = path.basename(image_path)
+    domain, extension = path.splitext(image_filename)
+
+    metadata_uri = f'{API_URL}/domains/{domain}'
+    proxy_uri = f'{IMAGEPROXY_URL}/https://{domain}/favicon.ico'
+
+    data = b'not_an_image'
+    mime_type = 'image/x-icon'
+
+    responses.add(responses.GET, metadata_uri, json={'image_src': None})
+    responses.add(responses.GET, proxy_uri, body=data, content_type=mime_type)
+
+
+@responses.activate
+@pytest.mark.parametrize('image_path', ['example.com.ico'])
+def test_domain_request(client, image_path, domain_responses):
+    response = client.get(f'/domains/{image_path}')
+
+    assert response.status_code == 200
+    assert response.data is not None
+    assert response.headers['Content-Type'] == 'image/x-icon'
+
+
+@pytest.fixture
+def recipe_responses(image_path):
     image_filename = path.basename(image_path)
     recipe_id, extension = path.splitext(image_filename)
 
@@ -25,7 +50,7 @@ def imageproxy_response(image_path):
 
 @responses.activate
 @pytest.mark.parametrize('image_path', ['image.png'])
-def test_recipe_request(client, image_path, imageproxy_response):
+def test_recipe_request(client, image_path, recipe_responses):
     response = client.get(f'/recipes/{image_path}')
 
     assert response.status_code == 200
--- web/app.py
+++ web/app.py
@@ -23,6 +23,37 @@ requests.sessions.Session.request = request_patch
 
 app = Flask(__name__)
 
+with open('web/data/empty.ico', 'rb') as f:
+    empty_icon = f.read()
+
+
+@app.route('/domains/<image_filename>')
+def domain(image_filename):
+    domain, extension = path.splitext(image_filename)
+
+    recipe = requests.get(
+        url=f'http://backend-service/domains/{domain}',
+        proxies={}
+    )
+    try:
+        recipe.raise_for_status()
+    except Exception:
+        return abort(404)
+
+    image_src = recipe.json().get('image_src')
+    image_src = image_src or f'https://{domain}/favicon.ico'
+    image = requests.get(
+        url=f'http://imageproxy/{image_src}',
+        proxies={}
+    )
+
+    try:
+        image.raise_for_status()
+    except Exception:
+        return empty_icon, 200, {'Content-Type': 'image/x-icon'}
+
+    return image.content, 200, {'Content-Type': image.headers['Content-Type']}
+
 
 @app.route('/recipes/<image_filename>')
 def recipe(image_filename):
