ee4348276c4da1638cbc43c953d021fd21d4272e
James Addison <james@reciperadar.com>
2020-10-16 11:41:16 +0100

    Add domain icon retrieval endpoint

--- web/app.py
+++ web/app.py
@@ -24,6 +24,33 @@ requests.sessions.Session.request = request_patch
 app = Flask(__name__)
 
 
+@app.route('/domains/<image_filename>')
+def domain(image_filename):
+    domain, extension = path.splitext(image_filename)
+
+    recipe = requests.get(
+        url=f'http://backend-service/domains/{domain}',
+        proxies={}
+    )
+    try:
+        recipe.raise_for_status()
+    except Exception:
+        return abort(404)
+
+    image_src = recipe.json().get('image_src')
+    image = requests.get(
+        url=f'http://imageproxy/{image_src}',
+        proxies={}
+    )
+
+    try:
+        image.raise_for_status()
+    except Exception:
+        return abort(404)
+
+    return image.content, 200, {'Content-Type': image.headers['Content-Type']}
+
+
 @app.route('/recipes/<image_filename>')
 def recipe(image_filename):
     recipe_id, extension = path.splitext(image_filename)
