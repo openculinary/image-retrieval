87cdec466ef083283962e2b69ee6979df6a0f082
James Addison <james@reciperadar.com>
2022-07-11 01:23:22 +0100

    Apply 'black' code formatting

--- tests/test_root.py
+++ tests/test_root.py
@@ -3,8 +3,8 @@ import pytest
 import responses
 
 
-API_URL = 'http://backend-service'
-IMAGEPROXY_URL = 'http://imageproxy'
+API_URL = "http://backend-service"
+IMAGEPROXY_URL = "http://imageproxy"
 
 
 @pytest.fixture
@@ -12,24 +12,24 @@ def domain_responses(image_path):
     image_filename = path.basename(image_path)
     domain, extension = path.splitext(image_filename)
 
-    metadata_uri = f'{API_URL}/domains/{domain}'
-    proxy_uri = f'{IMAGEPROXY_URL}/https://{domain}/favicon.ico'
+    metadata_uri = f"{API_URL}/domains/{domain}"
+    proxy_uri = f"{IMAGEPROXY_URL}/https://{domain}/favicon.ico"
 
-    data = b'not_an_image'
-    mime_type = 'image/x-icon'
+    data = b"not_an_image"
+    mime_type = "image/x-icon"
 
-    responses.add(responses.GET, metadata_uri, json={'image_src': None})
+    responses.add(responses.GET, metadata_uri, json={"image_src": None})
     responses.add(responses.GET, proxy_uri, body=data, content_type=mime_type)
 
 
 @responses.activate
-@pytest.mark.parametrize('image_path', ['example.com.ico'])
+@pytest.mark.parametrize("image_path", ["example.com.ico"])
 def test_domain_request(client, image_path, domain_responses):
-    response = client.get(f'/domains/{image_path}')
+    response = client.get(f"/domains/{image_path}")
 
     assert response.status_code == 200
     assert response.data is not None
-    assert response.headers['Content-Type'] == 'image/x-icon'
+    assert response.headers["Content-Type"] == "image/x-icon"
 
 
 @pytest.fixture
@@ -37,22 +37,22 @@ def recipe_responses(image_path):
     image_filename = path.basename(image_path)
     recipe_id, extension = path.splitext(image_filename)
 
-    metadata_uri = f'{API_URL}/recipes/{recipe_id}'
-    image_uri = 'http://example.org/image.png'
-    proxy_uri = f'{IMAGEPROXY_URL}/192,png/{image_uri}'
+    metadata_uri = f"{API_URL}/recipes/{recipe_id}"
+    image_uri = "http://example.org/image.png"
+    proxy_uri = f"{IMAGEPROXY_URL}/192,png/{image_uri}"
 
-    data = b'not_an_image'
-    mime_type = 'image/png'
+    data = b"not_an_image"
+    mime_type = "image/png"
 
-    responses.add(responses.GET, metadata_uri, json={'image_src': image_uri})
+    responses.add(responses.GET, metadata_uri, json={"image_src": image_uri})
     responses.add(responses.GET, proxy_uri, body=data, content_type=mime_type)
 
 
 @responses.activate
-@pytest.mark.parametrize('image_path', ['image.png'])
+@pytest.mark.parametrize("image_path", ["image.png"])
 def test_recipe_request(client, image_path, recipe_responses):
-    response = client.get(f'/recipes/{image_path}')
+    response = client.get(f"/recipes/{image_path}")
 
     assert response.status_code == 200
     assert response.data is not None
-    assert response.headers['Content-Type'] == 'image/png'
+    assert response.headers["Content-Type"] == "image/png"
--- web/app.py
+++ web/app.py
@@ -5,78 +5,66 @@ import requests
 
 
 def request_patch(self, *args, **kwargs):
-    kwargs['proxies'] = kwargs.pop('proxies', {
-        'http': 'http://proxy:3128',
-        'https': 'http://proxy:3128',
-    })
-    kwargs['timeout'] = kwargs.pop('timeout', 5)
-    kwargs['verify'] = kwargs.pop('verify', '/etc/ssl/k8s/proxy-cert/ca.crt')
+    kwargs["proxies"] = kwargs.pop(
+        "proxies",
+        {
+            "http": "http://proxy:3128",
+            "https": "http://proxy:3128",
+        },
+    )
+    kwargs["timeout"] = kwargs.pop("timeout", 5)
+    kwargs["verify"] = kwargs.pop("verify", "/etc/ssl/k8s/proxy-cert/ca.crt")
     return self.request_orig(*args, **kwargs)
 
 
-setattr(
-    requests.sessions.Session, 'request_orig',
-    requests.sessions.Session.request
-)
+setattr(requests.sessions.Session, "request_orig", requests.sessions.Session.request)
 requests.sessions.Session.request = request_patch
 
 
 app = Flask(__name__)
 
-with open('web/data/empty.ico', 'rb') as f:
+with open("web/data/empty.ico", "rb") as f:
     empty_icon = f.read()
 
 
-@app.route('/domains/<image_filename>')
+@app.route("/domains/<image_filename>")
 def domain(image_filename):
     domain, extension = path.splitext(image_filename)
 
-    recipe = requests.get(
-        url=f'http://backend-service/domains/{domain}',
-        proxies={}
-    )
+    recipe = requests.get(url=f"http://backend-service/domains/{domain}", proxies={})
     try:
         recipe.raise_for_status()
     except Exception:
         return abort(404)
 
-    image_src = recipe.json().get('image_src')
-    image_src = image_src or f'https://{domain}/favicon.ico'
-    image = requests.get(
-        url=f'http://imageproxy/{image_src}',
-        proxies={}
-    )
+    image_src = recipe.json().get("image_src")
+    image_src = image_src or f"https://{domain}/favicon.ico"
+    image = requests.get(url=f"http://imageproxy/{image_src}", proxies={})
 
     try:
         image.raise_for_status()
     except Exception:
-        return empty_icon, 200, {'Content-Type': 'image/x-icon'}
+        return empty_icon, 200, {"Content-Type": "image/x-icon"}
 
-    return image.content, 200, {'Content-Type': image.headers['Content-Type']}
+    return image.content, 200, {"Content-Type": image.headers["Content-Type"]}
 
 
-@app.route('/recipes/<image_filename>')
+@app.route("/recipes/<image_filename>")
 def recipe(image_filename):
     recipe_id, extension = path.splitext(image_filename)
 
-    recipe = requests.get(
-        url=f'http://backend-service/recipes/{recipe_id}',
-        proxies={}
-    )
+    recipe = requests.get(url=f"http://backend-service/recipes/{recipe_id}", proxies={})
     try:
         recipe.raise_for_status()
     except Exception:
         return abort(404)
 
-    image_src = recipe.json().get('image_src')
-    image = requests.get(
-        url=f'http://imageproxy/192,png/{image_src}',
-        proxies={}
-    )
+    image_src = recipe.json().get("image_src")
+    image = requests.get(url=f"http://imageproxy/192,png/{image_src}", proxies={})
 
     try:
         image.raise_for_status()
     except Exception:
         return abort(404)
 
-    return image.content, 200, {'Content-Type': image.headers['Content-Type']}
+    return image.content, 200, {"Content-Type": image.headers["Content-Type"]}
