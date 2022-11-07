bd5d03aa36e39d594a933c607ff6a51f0c46d435
James Addison <james@reciperadar.com>
2022-11-07 17:09:05 +0000

    Test refactor: provide a way to configure additional response contents to the recipe/domain response fixtures

--- tests/test_root.py
+++ tests/test_root.py
@@ -6,9 +6,7 @@ API_URL = "http://backend-service"
 IMAGEPROXY_URL = "http://imageproxy"
 
 
-@pytest.fixture
-@pytest.mark.respx
-def domain_responses(image_path, respx_mock):
+def domain_responses(image_path, respx_mock, **response_data):
     image_filename = path.basename(image_path)
     domain, extension = path.splitext(image_filename)
 
@@ -18,13 +16,21 @@ def domain_responses(image_path, respx_mock):
     data = b"not_an_image"
     mime_type = "image/x-icon"
 
-    response = {"image_src": None, "image_enabled": True}
+    response = {"image_src": None}
+    response.update(response_data)
+
     respx_mock.get(metadata_uri).respond(json=response)
     respx_mock.get(proxy_uri).respond(content=data, headers={"Content-Type": mime_type})
 
 
+@pytest.fixture
+@pytest.mark.respx
+def domain_responses_enabled(image_path, respx_mock):
+    return domain_responses(image_path, respx_mock, enabled=True)
+
+
 @pytest.mark.parametrize("image_path", ["example.com.ico"])
-def test_domain_request(client, image_path, domain_responses):
+def test_domain_request(client, image_path, domain_responses_enabled):
     response = client.get(f"/domains/{image_path}")
 
     assert response.status_code == 200
@@ -32,9 +38,7 @@ def test_domain_request(client, image_path, domain_responses):
     assert response.headers["Content-Type"] == "image/x-icon"
 
 
-@pytest.fixture
-@pytest.mark.respx
-def recipe_responses(image_path, respx_mock):
+def recipe_responses(image_path, respx_mock, **response_data):
     image_filename = path.basename(image_path)
     recipe_id, extension = path.splitext(image_filename)
 
@@ -45,13 +49,21 @@ def recipe_responses(image_path, respx_mock):
     data = b"not_an_image"
     mime_type = "image/png"
 
-    response = {"image_src": image_uri, "image_enabled": True}
+    response = {"image_src": image_uri}
+    response.update(response_data)
+
     respx_mock.get(metadata_uri).respond(json=response)
     respx_mock.get(proxy_uri).respond(content=data, headers={"Content-Type": mime_type})
 
 
+@pytest.fixture
+@pytest.mark.respx
+def recipe_responses_enabled(image_path, respx_mock):
+    return recipe_responses(image_path, respx_mock, enabled=True)
+
+
 @pytest.mark.parametrize("image_path", ["image.png"])
-def test_recipe_request(client, image_path, recipe_responses):
+def test_recipe_request(client, image_path, recipe_responses_enabled):
     response = client.get(f"/recipes/{image_path}")
 
     assert response.status_code == 200
