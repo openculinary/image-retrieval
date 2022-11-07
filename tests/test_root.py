2e4074fcd9fd1f4c49b850b056390ab5623a28ba
James Addison <james@reciperadar.com>
2022-11-07 17:18:15 +0000

    Add test coverage to ensure that empty icons and images are returned (for domains and recipes respectively) when image handling configuration is undefined

--- tests/test_root.py
+++ tests/test_root.py
@@ -5,6 +5,9 @@ import pytest
 API_URL = "http://backend-service"
 IMAGEPROXY_URL = "http://imageproxy"
 
+EMPTY_ICON = open("tests/data/empty.ico", "rb").read()
+EMPTY_IMAGE = open("tests/data/empty.png", "rb").read()
+
 
 def domain_responses(image_path, respx_mock, **response_data):
     image_filename = path.basename(image_path)
@@ -29,12 +32,27 @@ def domain_responses_enabled(image_path, respx_mock):
     return domain_responses(image_path, respx_mock, enabled=True)
 
 
+@pytest.fixture
+@pytest.mark.respx
+def domain_responses_undefined(image_path, respx_mock):
+    return domain_responses(image_path, respx_mock)
+
+
 @pytest.mark.parametrize("image_path", ["example.com.ico"])
 def test_domain_request(client, image_path, domain_responses_enabled):
     response = client.get(f"/domains/{image_path}")
 
     assert response.status_code == 200
     assert response.data is not None
+    assert response.headers["Content-Type"] == "image/x-icon"
+
+
+@pytest.mark.parametrize("image_path", ["example.com.ico"])
+def test_domain_request_image_undefined(client, image_path, domain_responses_undefined):
+    response = client.get(f"/domains/{image_path}")
+
+    assert response.status_code == 200
+    assert response.data == EMPTY_ICON
     assert response.headers["Content-Type"] == "image/x-icon"
 
 
@@ -62,10 +80,25 @@ def recipe_responses_enabled(image_path, respx_mock):
     return recipe_responses(image_path, respx_mock, enabled=True)
 
 
+@pytest.fixture
+@pytest.mark.respx
+def recipe_responses_undefined(image_path, respx_mock):
+    return recipe_responses(image_path, respx_mock)
+
+
 @pytest.mark.parametrize("image_path", ["image.png"])
 def test_recipe_request(client, image_path, recipe_responses_enabled):
     response = client.get(f"/recipes/{image_path}")
 
     assert response.status_code == 200
     assert response.data is not None
+    assert response.headers["Content-Type"] == "image/png"
+
+
+@pytest.mark.parametrize("image_path", ["image.png"])
+def test_recipe_request_image_undefined(client, image_path, recipe_responses_undefined):
+    response = client.get(f"/recipes/{image_path}")
+
+    assert response.status_code == 200
+    assert response.data == EMPTY_IMAGE
     assert response.headers["Content-Type"] == "image/png"
