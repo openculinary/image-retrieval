f6060cdabda0b4039cb35550585e3d9db1b79275
James Addison <james@reciperadar.com>
2024-08-15 16:15:29 +0100

    Fixup: move `httpx.get` calls within `try` code blocks

--- web/app.py
+++ web/app.py
@@ -23,23 +23,21 @@ with open("web/data/empty.ico", "rb") as f:
 
 @app.route("/domains/<image_filename>")
 def domain(image_filename):
-    domain_name, extension = path.splitext(image_filename)
-
-    response = httpx.get(f"http://backend-service/domains/{domain_name}", proxy=None)
+    domain, extension = path.splitext(image_filename)
     try:
+        response = httpx.get(f"http://backend-service/domains/{domain}", proxy=None)
         response.raise_for_status()
     except Exception:
         return abort(404)
-    domain = response.json()
 
-    if not domain.get("image_enabled"):
+    domain_info = response.json()
+    if not domain_info.get("image_enabled"):
         return empty_icon, 200, {"Content-Type": "image/x-icon"}
 
-    image_src = domain.get("image_src")
-    image_src = image_src or f"https://{domain_name}/favicon.ico"
-    image = httpx.get(url=f"http://imageproxy/{image_src}", proxy=None)
-
+    image_src = domain_info.get("image_src")
+    image_src = image_src or f"https://{domain}/favicon.ico"
     try:
+        image = httpx.get(url=f"http://imageproxy/{image_src}", proxy=None)
         image.raise_for_status()
     except Exception:
         return empty_icon, 200, {"Content-Type": "image/x-icon"}
@@ -54,21 +52,19 @@ with open("web/data/empty.png", "rb") as f:
 @app.route("/recipes/<image_filename>")
 def recipe(image_filename):
     recipe_id, extension = path.splitext(image_filename)
-
-    response = httpx.get(f"http://backend-service/recipes/{recipe_id}", proxy=None)
     try:
+        response = httpx.get(f"http://backend-service/recipes/{recipe_id}", proxy=None)
         response.raise_for_status()
     except Exception:
         return abort(404)
-    recipe = response.json()
 
+    recipe = response.json()
     if not recipe.get("image_enabled"):
         return empty_image, 200, {"Content-Type": "image/png"}
 
     image_src = recipe.get("image_src")
-    image = httpx.get(f"http://imageproxy/192,png/{image_src}", proxy=None)
-
     try:
+        image = httpx.get(f"http://imageproxy/192,png/{image_src}", proxy=None)
         image.raise_for_status()
     except Exception:
         return abort(404)
