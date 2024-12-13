b2b6b81b2d61b12912545944587e08c67621a247
James Addison <james@reciperadar.com>
2024-12-13 19:19:43 +0000

    Compatibility fixup/workaround: migrate to `HTTPXMocker`
    Ref: lundberg/respx#277

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import respx
 
 from web.app import app
 
@@ -6,3 +7,10 @@ from web.app import app
 @pytest.fixture
 def client():
     return app.test_client()
+
+
+@pytest.fixture()
+def respx_mock(request):
+    """Workaround: override the default plugin router"""
+    with respx.mock(using="httpx", assert_all_called=False) as respx_mock:
+        yield respx_mock
